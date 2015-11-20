31df3a4f42dae744446d79cb5e7fc4ee9d0164d5
vipulism <vipul0809@gmail.com>
2015-11-20 11:16:51 +0530

    using mysqli

--- json.php
+++ json.php
@@ -2,16 +2,22 @@
 
 	header('Content-type:application/json');
 
-	mysql_connect('localhost', 'root', '') or die( mysql_error());
-	
-	
-	mysql_select_db('company')  or die( mysql_error());
 
-	$select = mysql_query("SELECT * FROM `employee`");
+$db = new mysqli('localhost', 'root', '', 'company');
+
+$result = $db->query("SELECT * FROM `employee`");
+
+
+//	mysql_connect('localhost', 'root', '') or die( mysql_error());
+	
+	
+//	mysql_select_db('company')  or die( mysql_error());
+
+	//$select = mysql_query("SELECT * FROM `employee`");
  
 	$rows=array();
 
-	while($row=mysql_fetch_array($select)) 
+	while($row=mysqli_fetch_array($result)) 
 	{
 		$rows[] = $row;
 	}
